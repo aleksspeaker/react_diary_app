dd18adc70835c5615e3356c2eed1a5163720ca41
aleksspeaker <aleksspeaker@gmail.com>
2018-03-18 11:43:27 +0200

    Init commit

---
+++ src/components/genericButton.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GenericButton extends Component {
+  render() {
+    return <button className="Button Red"> Button </button>;
+  }
+}
+
+export default GenericButton;
